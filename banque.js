7ad8ce0a5976a21f1e3d61a727ffc95389df5643
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-25 13:01:21 +0100

    envoie XmlHttpRequest de l'eval js

--- banque.js
+++ banque.js
@@ -6,10 +6,12 @@ function verifLengthcompte()
     if (document.getElementById("compte").length < 16  || document.getElementById("compte").length > 16 )
     {
         alert("Attention : Votre compte doit contenir 16 lettres au minimum et au maximum !");
+        console.log(1)
         return compteTF;
     }
     else
     {
+        console.log(2)
         return compteTF=true;
     }
 }
@@ -19,19 +21,31 @@ function verifLengthsomme()
     if (document.getElementById("somme").length < 0 )
     {
         alert("Attention : Votre somme déposer doit être supérieur à 0€ au minimum!");
+        console.log(3);
         return sommeTF;
     }
     else
     {
         document.getElementById("somme").style.backgroundColor = "#6eff33";
+        console.log(4);
         return sommeTF=true;
     }
 }
 
 function alertIsTrue(verifLengthcompte,verifLengthsomme){
     if(compteTF==true &&sommeTF==true){
+        console.log(5);
         return alert ("La somme de XXX à bien été déposée.");
     }else{
+        console.log(6);
         return alert ("Erreur sur le champs compte ou dans le compte somme.");
     }
+}
+
+let envoie = new XMLHttpRequest();
+envoie.open('GET', 'http://exjs.apigame.co/data.php');
+envoie.send();
+envoie.onload= function(){
+    document.getElementById("somme");
+    document.getElementById("compte");
 }
