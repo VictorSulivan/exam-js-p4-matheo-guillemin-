ac5763d258bb8ea22b1a5fcc6ac607598d043aeb
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-25 11:39:22 +0100

    deuxieme commit pour la validation des champs a remplir

--- banque.js
+++ banque.js
@@ -1,2 +1,21 @@
 var exam_formulaire = document.getElementById("exam-form");
 
+function W3docs(){ 
+    var compte = document.forms["exam-form"]["compte"];               
+    var somme = document.forms["exam-form"]["somme"]; 
+
+    if (compte.value == "")                                  
+    { 
+        alert("Mettez votre compte."); 
+        compte.focus(); 
+        return false; 
+    }    
+    if (somme.value == "")                               
+    { 
+        alert("Mettez votre somme à envoyer."); 
+        somme.focus(); 
+        return false; 
+    }
+    return true; 
+}
+
