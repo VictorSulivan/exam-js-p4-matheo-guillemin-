433bd24d5c54129937c68fec4d26672580fd3606
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-25 11:45:13 +0100

    correction todo2 pour la verification des champs de saisie du formulaire

--- banque.js
+++ banque.js
@@ -1,21 +1,27 @@
 var exam_formulaire = document.getElementById("exam-form");
 
-function W3docs(){ 
-    var compte = document.forms["exam-form"]["compte"];               
-    var somme = document.forms["exam-form"]["somme"]; 
-
-    if (compte.value == "")                                  
-    { 
-        alert("Mettez votre compte."); 
-        compte.focus(); 
-        return false; 
-    }    
-    if (somme.value == "")                               
-    { 
-        alert("Mettez votre somme à envoyer."); 
-        somme.focus(); 
-        return false; 
+function verifLengthcompte()
+{
+    if (document.getElementById("compte").value.length < 16  || document.getElementById("compte").value.length > 16 )
+    {
+        alert("Attention : Votre compte doit contenir 16 lettres au minimum et au maximum !");
+        return false;
     }
-    return true; 
+    else
+    {
+        return true;
+    }
+}
+function verifLengthsomme()
+{
+    if (document.getElementById("compte").value.length < 0 )
+    {
+        alert("Attention : Votre somme déposer doit être supérieur à 0€ au minimum!");
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
 
