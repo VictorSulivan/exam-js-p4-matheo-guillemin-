f9b22bbd03989a8ee3aaa67c548c7acc61917479
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-25 11:58:49 +0100

    troisieme commit ou une couleur a ete mise sur le champ compte une fois la validation faite et reussite

--- banque.js
+++ banque.js
@@ -2,7 +2,7 @@ var exam_formulaire = document.getElementById("exam-form");
 
 function verifLengthcompte()
 {
-    if (document.getElementById("compte").value.length < 16  || document.getElementById("compte").value.length > 16 )
+    if (document.getElementById("compte").length < 16  || document.getElementById("compte").length > 16 )
     {
         alert("Attention : Votre compte doit contenir 16 lettres au minimum et au maximum !");
         return false;
@@ -14,13 +14,14 @@ function verifLengthcompte()
 }
 function verifLengthsomme()
 {
-    if (document.getElementById("compte").value.length < 0 )
+    if (document.getElementById("somme").length < 0 )
     {
         alert("Attention : Votre somme déposer doit être supérieur à 0€ au minimum!");
         return false;
     }
     else
     {
+        document.getElementById("somme").style.backgroundColor = "#6eff33";
         return true;
     }
 }
