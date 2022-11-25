84b795d4dc9a519a6c772c9d56bdcf2758c85bf7
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-25 12:05:49 +0100

    quatrieme commit sur l'envoie d'un message de confirmation de dépos d'argent

--- banque.js
+++ banque.js
@@ -2,27 +2,36 @@ var exam_formulaire = document.getElementById("exam-form");
 
 function verifLengthcompte()
 {
+    var compteTF=false;
     if (document.getElementById("compte").length < 16  || document.getElementById("compte").length > 16 )
     {
         alert("Attention : Votre compte doit contenir 16 lettres au minimum et au maximum !");
-        return false;
+        return compteTF;
     }
     else
     {
-        return true;
+        return compteTF=true;
     }
 }
 function verifLengthsomme()
 {
+    var sommeTF=false;
     if (document.getElementById("somme").length < 0 )
     {
         alert("Attention : Votre somme déposer doit être supérieur à 0€ au minimum!");
-        return false;
+        return sommeTF;
     }
     else
     {
         document.getElementById("somme").style.backgroundColor = "#6eff33";
-        return true;
+        return sommeTF=true;
     }
 }
 
+function alertIsTrue(verifLengthcompte,verifLengthsomme){
+    if(compteTF==true &&sommeTF==true){
+        return alert ("La somme de XXX à bien été déposée.");
+    }else{
+        return alert ("Erreur sur le champs compte ou dans le compte somme");
+    }
+}
